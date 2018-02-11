14f2f21ad036091b8119f80fcef6aba9aace79ff
Souzooka <bazookaharwell@gmail.com>
2018-02-11 01:34:12 -1000

    Write documentation for shift/unshit for MyArray

--- Array/js/shift.js
+++ Array/js/shift.js
@@ -1,11 +1,31 @@
+/*
+	MyArray.prototype.shift
+	Parameters:
+		none
+	Return value:
+		The first value of the array, or
+		undefined if the array is empty
+	Behavior/remarks:
+		Mutates the array by removing the first element and changing the index of
+		the remaining elements by -1.
+
+		Returns the element which was removed.
+*/
+
+
 module.exports = function shift() {
+	// Hold onto the element to be removed
 	let ret = this[0];
 
+	// Change the indexes of the remaining elements
 	for (let i = 0; i < this.length - 1; ++i) {
 		this[i] = this[i + 1];
 	}
 
+	// Delete the last element (now duplicate of penultimate element) and reassign length
 	delete this[this.length - 1];
 	this.length = Math.max(this.length - 1, 0);
+
+	// Return the removed element
 	return ret;
 }
--- Array/js/unshift.js
+++ Array/js/unshift.js
@@ -1,12 +1,31 @@
+/*
+	MyArray.prototype.unshift
+	Parameters:
+		([...any]): The values to prepend onto the array.
+	Return value:
+		The new length of the modified array.
+	Behavior/remarks:
+		Mutates an array by prepending value(s) onto the end of it.
+		Returns the new length of the array.
+
+		If there are no arguments given to the method,
+		the length of the array is just returned instead.
+*/
+
 module.exports = function unshift() {
+	// return this.length if no arguments are provided
 	if (arguments.length == 0) { return this.length; }
+
+	// shift all of the indexes in the current array to allocate room for arguments
 	for (let i = this.length - 1; i >= 0; --i) {
 		this[i + arguments.length] = this[i];
 	}
 
+	// assign each argument to its spot at the beginning of the array
 	for (let i = 0; i < arguments.length; ++i) {
 		this[i] = arguments[i];
 	}
 
+	// return the new length of the array
 	return this.length;
 }
