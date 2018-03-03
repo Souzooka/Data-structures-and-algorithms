0d1d9bbafd9c5b665d2397498aa2f5b822d4a67d
Souzooka <bazookaharwell@gmail.com>
2018-03-03 01:45:59 -1000

    More Stack constructor tests

--- Stack/js/spec/Stack_spec.js
+++ Stack/js/spec/Stack_spec.js
@@ -8,5 +8,20 @@ describe("Stack", function() {
 
       assert.strictEqual(stack.count, 0);
     });
+    it("should initialize a stack with data contained in enumerable if it is given as first argument", function() {
+      let stack = new Stack([1, 2, 3, 4]);
+      assert.strictEqual(stack.count, 4);
+      assert.deepEqual(stack.__collection__, [1, 2, 3, 4]);
+
+      stack = new Stack(new Set([1, 2, 3, 4]));
+      assert.strictEqual(stack.count, 4);
+      assert.deepEqual(stack.__collection__, [1, 2, 3, 4]);
+    });
+    it("should initialize an empty stack if given a non-enumerable first argument", function() {
+      assert.deepEqual(new Stack(1), new Stack());
+      assert.deepEqual(new Stack(null), new Stack());
+      assert.deepEqual(new Stack({0: "1", 1: 2}), new Stack());
+      assert.deepEqual(new Stack(NaN), new Stack());
+    });
   });
 });
